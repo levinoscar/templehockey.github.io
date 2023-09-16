de2c62acfba3425a76c9e2ce44bbfc85da6493b1
Oscar Levin <tuk85580@temple.edu>
2023-09-16 09:19:48 -0400

    First Temple Ice Hockey Commit

---
+++ levin_web_lab01/src/main/java/com/levin_web/LevinWebApplication.java
@@ -0,0 +1,13 @@
+package com.levin_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevinWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevinWebApplication.class, args);
+	}
+
+}
